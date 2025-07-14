dda0235586bc6254daffac2b3034710295f174a1
cent2 <torievemir100@gmail.com>
2025-07-14 10:29:27 +0300

    .

--- build/CMakeFiles/message-objs.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/message-objs.dir/D_/Software/Projects/VS_code/archiver/libs/stack/stack.c.obj"
-  "CMakeFiles/message-objs.dir/D_/Software/Projects/VS_code/archiver/libs/stack/stack.c.obj.d"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/message-objs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
